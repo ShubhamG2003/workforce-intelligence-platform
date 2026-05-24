be9690273fa0e86ae000cff0720d1e4f4637e0e5
Shubham G <141394414+ShubhamG2003@users.noreply.github.com>
2026-05-24 19:16:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DB_CONFIG = {
     "user": "insert_username",
-    "password": "DB_PASSWORD",
+    "password": DB_PASSWORD,
     "host": "localhost",
     "port": 5432,
     "db": "workforce_db"
